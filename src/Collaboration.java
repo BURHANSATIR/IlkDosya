a2afa9a04ed018ef4f42c211e824572f37beda31
BURHANSATIR <satirburhan@gmail.com>
2023-10-14 10:25:35 +0300

    Suatıma

--- src/Collaboration.java
+++ src/Collaboration.java
@@ -1,5 +1,11 @@
 public class Collaboration {
     public static void main(String[] args) {
         System.out.println("Merhaba Collaboration");
+        int a= 3;
+        int b= 5+a;
+        int c= a+b;
+
+        System.out.println("c = " + c);
+     
     }
 }
