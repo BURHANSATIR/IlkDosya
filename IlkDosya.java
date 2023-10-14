1f36296b1886fd782d9caf68d18e3f01bd91dba0
BURHANSATIR <satirburhan@gmail.com>
2023-10-14 09:16:08 +0300

    first commit

---
+++ IlkDosya.java
@@ -0,0 +1,15 @@
+public class IlkDosya {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+        /*
+
+    git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/BURHANSATIR/IlkDosya.git
+    git push -u origin main
+         */
+
+    }
+}
